708b65f6f29fa1a9bb13c5ce32da0521c51abca9
Manohar Reddy <143716600+MANOHARINAKOLLU@users.noreply.github.com>
2023-09-06 16:47:27 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,6 @@
+// home.js
+const ctaButton = document.querySelector('.cta-button');
+
+ctaButton.addEventListener('click', () => {
+    alert("Thank you for taking the first step! We'll be in touch soon.");
+});
